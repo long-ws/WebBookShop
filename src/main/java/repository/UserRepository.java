c931f39152799f9d0737eb14a25415f6efc2a9d7
Long <vvl.vvlong@gmail.com>
2026-05-11 17:42:38 +0700

    feat: Cập nhật thêm các service xử lý cho đăng nhập, đăng ký

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -5,14 +5,34 @@ import java.util.Optional;
 import beans.user.User;
 
 public interface UserRepository {
-    
-    /**
-     * Lưu user mới. 
-     */
-    long save(User user) throws SQLException;
-    
-    /**
-     * Tìm user theo ID
-     */
-    Optional<User> findById(long id);
+
+	/**
+	 * Lưu user mới.
+	 */
+	long save(User user) throws SQLException;
+
+	/**
+	 * Tìm user theo ID
+	 */
+	Optional<User> findById(long id);
+
+	/**
+	 * Tìm người dùng theo Username.
+	 */
+	Optional<User> findByUsername(String username);
+
+	/**
+	 * Tìm người dùng theo Email.
+	 */
+	Optional<User> findByEmail(String email);
+
+	/**
+	 * Kiểm tra xem Username đã tồn tại chưa.
+	 */
+	boolean existsByUsername(String username);
+
+	/**
+	 * Kiểm tra xem Email đã tồn tại chưa.
+	 */
+	boolean existsByEmail(String email);
 }
